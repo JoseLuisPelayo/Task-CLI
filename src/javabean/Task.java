cc71645b95b4ec9c60251c775c87e3a19da2d9f1
JoseLuisPelayo <joseluis.garcia893@comunidadunir.net>
2024-12-17 22:29:35 +0100

    ADD: Task Class

---
+++ src/javabean/Task.java
@@ -0,0 +1,74 @@
+package javabean;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    int id;
+    String description;
+    String status;
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
+
+    public Task() {
+    }
+
+    public Task(String description) {
+        this.description = description;
+        this.status = "not done";
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
